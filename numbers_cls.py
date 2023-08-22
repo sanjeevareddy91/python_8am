e9f39973ccfaa913e7c1182a5b8cab1c2819beee
sanjeevareddy91 <gsanjeevreddy91@gmail.com>
2023-08-22 08:39:08 +0530

    Added new file and updated requirments file

---
+++ numbers_cls.py
@@ -0,0 +1,24 @@
+# numbers: Numerical values.. 
+    # integers(int) - numeric value without decimal points
+    # floating(float) - numeric value with decimal point. 
+
+# a=73872783287387282823782873278
+
+# print(a)
+# print(type(a))
+
+# b=78778287283298892892.73287282
+
+# print(b)
+# print(type(b))
+
+# complex number : a+ib
+
+# a=3+4j
+
+# print(a)
+# print(type(a))
+
+# b = complex(3,4)
+
+# print(b)
